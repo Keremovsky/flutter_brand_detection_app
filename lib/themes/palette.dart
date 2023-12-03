0ef50c009a35b272be844768a283ea8e44c71404
Kerem Okumus <96080839+Keremovsky@users.noreply.github.com>
2023-12-03 20:56:02 +0300

    main structure, packages etc. set up

---
+++ lib/themes/palette.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Palette {
+  static const mainColor = Color.fromRGBO(87, 182, 240, 1);
+  static const green = Color.fromRGBO(30, 215, 55, 1);
+  static const yellow = Color.fromRGBO(255, 255, 39, 1);
+  static const red = Color.fromRGBO(255, 52, 54, 1);
+  static const grey = Color.fromRGBO(102, 102, 102, 1);
+}
