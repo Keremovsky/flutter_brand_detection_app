e4e2566341dffb2300e2d377bbe980f1f5248ed0
Kerem Okumus <96080839+Keremovsky@users.noreply.github.com>
2023-12-21 16:00:41 +0300

    reset password screen UI demo

---
+++ lib/features/auth/screens/reset_password_screen.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_brand_detection_app/core/constants/theme_constants.dart';
+import 'package:flutter_brand_detection_app/core/utils/custom_text_button.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+
+class ResetPasswordScreen extends ConsumerStatefulWidget {
+  const ResetPasswordScreen({super.key});
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() =>
+      _ResetPasswordScreenState();
+}
+
+class _ResetPasswordScreenState extends ConsumerState<ResetPasswordScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: IconButton(
+          onPressed: () {
+            context.pop();
+          },
+          icon: const Icon(
+            Icons.arrow_back,
+            size: ThemeConstants.generalIconSize,
+          ),
+        ),
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+          child: Column(
+            children: [
+              TextField(),
+              const SizedBox(height: 15),
+              TextField(),
+              const SizedBox(height: 30),
+              CustomTextButton(
+                text: Text(
+                  "Şifreyi yenile",
+                  style: Theme.of(context).textTheme.displayLarge,
+                ),
+                onTap: () {},
+                borderRadius: BorderRadius.circular(10),
+                height: 40,
+                width: 140,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
